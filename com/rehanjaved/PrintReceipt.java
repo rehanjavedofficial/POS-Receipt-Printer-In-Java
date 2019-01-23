c9052c4ad3ff9fc2a2d49bd722bfedd7fe0aa327
Rehan Javed <rehanjaved.official@gmail.com>
2019-01-24 01:31:02 +0500

    Complete Printable

--- com/rehanjaved/PrintReceipt.java
+++ com/rehanjaved/PrintReceipt.java
@@ -1,6 +1,9 @@
 package com.rehanjaved;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.print.PageFormat;
 import java.awt.print.Paper;
 import java.awt.print.Printable;
@@ -64,11 +67,42 @@ public class PrintReceipt implements Printable {
 	}
 	
 	/**
-	 * 
+	 * Printing with Graphics..
 	 */
 	@Override
 	public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException {
-		return 0;
+		
+		if (pageIndex > 0) {
+			return NO_SUCH_PAGE;
+		}
+		
+		Graphics2D g2d = (Graphics2D) graphics;
+		g2d.setColor(Color.black);
+		
+		g2d.setFont(new Font("Consolas", Font.BOLD, 12));
+		g2d.drawString(spaces(10)+"CITY AUTO", 4, 10);
+		g2d.setFont(new Font("Consolas", Font.PLAIN, 10));
+		g2d.drawString(spaces(4)+"Address..", 0, 22);
+
+		g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
+		
+		return PAGE_EXISTS;
+	
+	}
+
+	/**
+	 * Adding spaces into the num.
+	 * @param total spaces
+	 * @return all spaces in string.
+	 */
+	public String spaces(int num) {
+		
+		String sp = "";
+		for(int i = 0; i < num; i++)
+			sp += " ";
+		
+		return sp;
+		
 	}
 
 }
